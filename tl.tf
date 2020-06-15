22ecf5785ab92b2b2ce5a730a6146679d31e30cd
1398abhisingh911 <52207042+1398abhisingh911@users.noreply.github.com>
2020-06-15 07:46:32 +0530

    dd

--- tl.tf
+++ tl.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "tls_private_key" task1_p_key  {
+  algorithm = "RSA"
+}
+
+
+resource "aws_key_pair" "task1-key" {
+  key_name    = "task1-key"
+  public_key = tls_private_key.task1_p_key.public_key_openssh
+  }
+
+
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
 ingress {
@@ -34,16 +45,17 @@ tags = {
 }
 
 resource "aws_instance" "myos" {
+
   ami           = "ami-0447a12f28fddb066"
   instance_type = "t2.micro"
-  key_name = "day3"
+    key_name      = "task1-key"
   security_groups = [ "allow_http" ]
 
   connection {
     agent    = "false"
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("C:/Users/196AKS/Desktop/Cloud Intern/Terraform/task1/day3.pem")
+    private_key =  tls_private_key.task1_p_key.private_key_pem
     host     = aws_instance.myos.public_ip
   }
 
@@ -57,6 +69,8 @@ resource "aws_instance" "myos" {
       "sudo systemctl enable httpd",
     ]
   }
+
+ 
 
   tags = {
     Name = "cloud-os"
@@ -90,7 +104,7 @@ depends_on = [
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("C:/Users/196AKS/Desktop/Cloud Intern/Terraform/task1/day3.pem")
+     private_key =  tls_private_key.task1_p_key.private_key_pem
     host     = aws_instance.myos.public_ip
   }
 
@@ -107,7 +121,14 @@ provisioner "remote-exec" {
 resource "aws_s3_bucket" "b" {
   bucket = "1398abhisingh911"
   acl    = "public-read"
-
+   force_destroy  = true
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://b"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 tags = {
     Name = "1398abhisingh911"
   }
@@ -118,7 +139,7 @@ depends_on = [
   aws_s3_bucket.b,
 ]
   bucket       = "1398abhisingh911"
-  key          = "aa.jpg "
+   key        = "img"
   source       = "C:/Users/196AKS/Desktop/Cloud Intern/Terraform/task1/aa.jpg "
   acl          = "public-read"
   content_type = "image or jpg"
@@ -165,37 +186,64 @@ resource "aws_s3_bucket_policy" "example" {
   policy = data.aws_iam_policy_document.s3_policy.json
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-origin {
-    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-    }
-  }
-enabled             = true
-  is_ipv6_enabled     = true
-default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-forwarded_values {
-      query_string = false
-cookies {
-        forward = "none"
-      }
-    }
-viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+resource "aws_cloudfront_distribution" "distribution" {
+    origin {
+        domain_name = "${aws_s3_bucket.b.bucket_regional_domain_name}"
+        origin_id = "${aws_s3_bucket.b.bucket}"
+
+
+        custom_origin_config {
+            http_port = 80
+            https_port = 443
+            origin_protocol_policy = "match-viewer"
+            origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        }
 }
+ 
+    default_root_object = "aa.jpg"
+    enabled = true
+
+
+
+    default_cache_behavior {
+        allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "${aws_s3_bucket.b.bucket}"
+
+
+        #Not Forward all query strings, cookies and headers
+        forwarded_values {
+            query_string = false
+	    cookies {
+		forward = "none"
+	    }
+            
+        }
+
+
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+
+    # Distributes content to all
+    price_class = "PriceClass_All"
+
+
+    # Restricts who is able to access this content
+    restrictions {
+        geo_restriction {
+            # type of restriction, blacklist, whitelist or none
+            restriction_type = "none"
+        }
+    }
+
+
+    # SSL certificate for the service.
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
